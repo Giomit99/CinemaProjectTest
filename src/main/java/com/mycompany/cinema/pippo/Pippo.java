e9d7cafcbffb7dcc27ad6faad189ffd12979fa2e
Giomit99 <omitkov999@gmail.com>
2024-01-10 22:08:33 +0100

    Add Pippo e Pluto

---
+++ src/main/java/com/mycompany/cinema/pippo/Pippo.java
@@ -0,0 +1,11 @@
+package com.mycompany.cinema.pippo;
+
+import com.mycompany.cinema.pluto.Pluto;
+
+public class Pippo {
+    private Pluto pluto;
+
+    public Pippo(){
+        pluto= new Pluto();
+    }
+}
---
+++ src/main/java/com/mycompany/cinema/pluto/Pluto.java
@@ -0,0 +1,11 @@
+package com.mycompany.cinema.pluto;
+
+import com.mycompany.cinema.pippo.Pippo;
+
+public class Pluto {
+    private Pippo pippo;
+
+    public Pluto(){
+        pippo= new Pippo();
+    }
+}
